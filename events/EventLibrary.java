75c96dd50e8994c4c189363f620d93e239d6c7dd
Edelveysa <51116291+Edelveysa@users.noreply.github.com>
2020-12-25 23:49:47 +0300

    Update EventLibrary.java

--- events/EventLibrary.java
+++ events/EventLibrary.java
@@ -1,5 +1,6 @@
 package events;
 
+import exceptions.InvalidParameterException;
 import interfaces.EventManage;
 import kids.*;
 import story.OnceStoryObject;
@@ -9,14 +10,20 @@ import java.util.ArrayList;
 public class EventLibrary implements EventManage {
     private ArrayList<Kid> subscribers = new ArrayList<Kid>();
 
-    public EventLibrary(Kid[] witnesses){
+    public EventLibrary(Kid[] witnesses) throws InvalidParameterException{
         for(int i = 0; i< witnesses.length; i++){
+            if(witnesses[i] == null){
+                throw new InvalidParameterException("Несуществующий свидетель не может быть добавлен.");
+            }
             subscribers.add(witnesses[i]);
         }
     }
 
     @Override
-    public void addEvent(Object o){
+    public void addEvent(Object o) throws InvalidParameterException{
+        if (o == null){
+            throw new InvalidParameterException("Попытка добавить новый объект.");
+        }
         if (o instanceof OnceStoryObject.Balloon){
             subscribers
                     .stream()
@@ -33,6 +40,12 @@ public class EventLibrary implements EventManage {
 
     @Override
     public boolean checkEventIn(Object event, Object witness){
+        if(event == null){
+            throw new InvalidParameterException("Попытка проверить отсутствующее событие.");
+        }
+        if(witness == null){
+            throw new InvalidParameterException("Несуществующий человек не может иметь воспоминаний.");
+        }
             if (witness instanceof Citizen && event instanceof Mechanic){
                 Citizen citizen = (Citizen) witness;
                 Mechanic mechanic = (Mechanic) event;
