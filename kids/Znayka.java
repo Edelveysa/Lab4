80859c2a03316d5b1498f41391257988be16c2b1
Edelveysa <51116291+Edelveysa@users.noreply.github.com>
2020-12-25 22:29:55 +0300

    Update Znayka.java

--- kids/Znayka.java
+++ kids/Znayka.java
@@ -4,6 +4,7 @@ import enums.Location;
 import events.EventLibrary;
 import exceptions.BadEndException;
 import interfaces.AbleToFind;
+import story.Legend;
 
 import java.util.Arrays;
 import java.util.Objects;
@@ -18,6 +19,7 @@ public final class Znayka extends Kid implements AbleToFind {
     public void find() {
         System.out.println(getName() + " решил найти своих улетевших товарищей");
         setLocation(Location.ZMEEVKA);
+
     }
 
     public void findResult(boolean[] memoryFind) throws BadEndException {
@@ -40,18 +42,25 @@ public final class Znayka extends Kid implements AbleToFind {
 
     }
 
-    public void find(Citizen citizen, EventLibrary eventLibrary) throws BadEndException {
+    public void find(Citizen citizen, EventLibrary eventLibrary) throws BadEndException, NullPointerException {
+
         find();
-        boolean[] memoryFind = new boolean[3];
+        boolean[] memoryFind = {false, false, false};
         int j = 0;
-        for (int i = 0; i<citizen.getAllMemory().length; i++){
-            if(citizen.getAllMemory()[i] != null && eventLibrary.checkInEvents(citizen.getAllMemory()[i])){
-                memoryFind[j] = true;
+        Object[] temp = getMyMemory().toArray();
+        for(int i = 0; i< temp.length; i++){
+            try{
+            if(eventLibrary.checkEventIn(temp[i], citizen)){
+                memoryFind[j]=true;
                 j++;
             }
+            }catch (NullPointerException nullPointerException){
+                if(eventLibrary.checkEventIn(new Legend(), citizen)){
+                    memoryFind[j]=true;
+                    j++;
+                }
+            }
         }
-
-
         findResult(memoryFind);
     }
 
