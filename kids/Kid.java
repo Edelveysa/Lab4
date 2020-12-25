c5e2409878fae4e77551b5ea3b8255880d8f4014
Edelveysa <51116291+Edelveysa@users.noreply.github.com>
2020-12-25 22:29:24 +0300

    Update Kid.java

--- kids/Kid.java
+++ kids/Kid.java
@@ -1,8 +1,11 @@
 package kids;
 
 import enums.Location;
+import exceptions.NoLegendException;
 import interfaces.AbleToUpdate;
 import interfaces.EventManage;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Objects;
 
@@ -10,6 +13,7 @@ public abstract class Kid implements AbleToUpdate{
     private String name;
     private Location location;
     private String[] memory;
+    private ArrayList<Object> myMemory = new ArrayList<Object>();
 
     public Kid(String name, Location location){
         this.name = name;
@@ -18,20 +22,10 @@ public abstract class Kid implements AbleToUpdate{
     }
 
     @Override
-    public void update(String remembrance) {
-            boolean addIn = false;
-            for(int i = 0; i < memory.length; i++) {
-                if (memory[i] == null) {
-                    memory[i] = remembrance;
-                    addIn = true;
-                    break;
-                }
-            }
-            if (addIn = false){
-                System.out.println("Память заполнена");
-            }
+    public void update(Object o) {
+        this.myMemory.add(o);
+    }
 
-        }
 
     public String getName() {
         return name;
@@ -44,8 +38,28 @@ public abstract class Kid implements AbleToUpdate{
         System.out.println(getName() + " оказался в " + location.getLocation());
     }
 
+    public ArrayList<Object> getMyMemory() {
+        return myMemory;
+    }
+
     public String[] getAllMemory() {
         return memory;
+    }
+
+    public boolean believeInLegend(Kid kid){
+        ArrayList<Object> temp = getMyMemory();
+        Gvozdik gvozdik = (Gvozdik) kid;
+        try{
+            if(temp.contains(gvozdik)){
+                return true;
+            }else{
+                throw new NoLegendException();
+            }
+        }catch(NoLegendException noLegendException){
+            return false;
+        }
+
+
     }
 
     @Override
