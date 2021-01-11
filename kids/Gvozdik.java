2adb76591fc454bc1e20c8db3254991dc7e2f3d6
Delsa <51116291+Edelveysa@users.noreply.github.com>
2021-01-11 17:33:57 +0300

    Update Gvozdik.java

--- kids/Gvozdik.java
+++ kids/Gvozdik.java
@@ -1,10 +1,13 @@
 package kids;
 
 import enums.Location;
+import interfaces.EventManage;
 
 public class Gvozdik extends Kid{
+
     public Gvozdik(){
         super("Гвоздик", Location.ZMEEVKA);
+        EventManage.addEvent(this);
     }
 
 }
