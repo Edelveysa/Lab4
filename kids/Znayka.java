f6c6e81fd9b0b331d52747d418cd77a0f380f5ed
Delsa <51116291+Edelveysa@users.noreply.github.com>
2021-01-11 17:32:35 +0300

    Update Znayka.java

--- kids/Znayka.java
+++ kids/Znayka.java
@@ -1,11 +1,11 @@
 package kids;
 
 import enums.Location;
-import events.EventLibrary;
 import exceptions.BadEndException;
-import exceptions.InvalidParameterException;
 import interfaces.AbleToFind;
-import story.Legend;
+import interfaces.AbleToUpdate;
+import interfaces.EventManage;
+import story.OnceStoryObject;
 
 import java.util.Arrays;
 import java.util.Objects;
@@ -14,6 +14,16 @@ public final class Znayka extends Kid implements AbleToFind {
 
     public Znayka() {
         super("Знайка", Location.FLOWERTOWN);
+        AbleToUpdate able = new AbleToUpdate(){
+            @Override
+            public void update(Object event) {
+                if(event instanceof Mechanic || event instanceof OnceStoryObject.Balloon){
+                    Znayka.this.getMyMemory().add(event);
+                }
+            }
+        };
+        EventManage.addSubscriber(able);
+
     }
 
     @Override
@@ -23,41 +33,39 @@ public final class Znayka extends Kid implements AbleToFind {
 
     }
 
-    public void findResult(boolean[] memoryFind) throws BadEndException {
-        int temp = 0;
-        for (int i = 0; i<memoryFind.length; i++){
-            if(memoryFind[i] == true){
-                temp++;
-            }
-        }
-        try{
-            if(temp == 3){
-                System.out.println("Поиски увенчались успехом!");
-            }else{
-                throw new BadEndException();
-            }
-
-        }catch (BadEndException e){
-            System.out.println("...их не нашли.");
-        }
-
-    }
-
-    public void find(Citizen citizen, EventLibrary eventLibrary) throws BadEndException {
+    public void find(Citizen citizen) throws BadEndException {
         find();
-        boolean[] memoryFind = {false, false, false};
-        int j = 0;
-        Object[] temp = getMyMemory().toArray();
-        for(int i = 0; i< temp.length; i++){
-            if(eventLibrary.checkEventIn(temp[i], citizen)){
-                memoryFind[j]=true;
-                j++;
+        boolean[] check = checkMemoryZnayka(citizen);
+        int temp = 0;
+        for(int i = 0; i<check.length; i++){
+            try {
+                if (check[i]){
+                    throw new BadEndException();
+                } else{
+                    temp++;
+                }
+            } catch (BadEndException badEndException){
+                System.out.println("Их не могут найти...");
             }
+
+            if (temp == 3){
+                System.out.println("Поиски увенчались успехом!");
+            }
+
         }
-        findResult(memoryFind);
     }
 
-
+    boolean[] checkMemoryZnayka(Citizen citizen){
+        boolean[] temp = {false, false, false};
+        int j = 0;
+        for (int i = 0; i<getMyMemory().size(); i++){
+                    if(citizen.getMyMemory().indexOf(getMyMemory().get(i)) != -1){
+                        temp[j] = true;
+                        j++;
+                    }
+                };
+        return temp;
+    }
 
     @Override
     public boolean equals(Object o) {
